e13a5cf8352734debd0b893870104a2b0dcaa888
James Colestock <james@colestock.com>
2020-10-20 10:14:20 -0600

    added fields to database, RESTful endpoints; updated index.html with project info, etc.

--- app.py
+++ app.py
@@ -1,36 +1,55 @@
 import os
-
-from flask import make_response, jsonify, Flask, request, render_template
 import sqlite3
 
+from flask import make_response, jsonify, Flask, request, render_template
+
 app = Flask(__name__)
+
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html', app_name=app.name)
 
+
 @app.route('/api/todo/', methods=['POST'])
 @app.route('/api/todo/<int:id>', methods=['GET'])
 def todo(id=None):
     conn = sqlite3.connect('rest.db', isolation_level=None)
+    conn.row_factory = dict_factory
     c = conn.cursor()
     if request.method == 'GET':
-        c.execute('SELECT * FROM todos WHERE id = %s' % id)
-        todo_data = c.fetchone()
-        res = make_response(jsonify(todo_data))
+        c.execute("SELECT id, user_id AS userId, title, completed FROM todos WHERE id = %s" % id)
+        data = c.fetchone()
+        data['completed'] = bool(data['completed'])
+        res = make_response(jsonify(data))
         res.mimetype = 'application/json'
         c.close()
         conn.close()
         return res
     elif request.method == 'POST':
         payload = request.get_json()
-        sql = 'INSERT INTO todos (description) VALUES ("{0}")'.format(payload['description'])
+        data = tuple([payload['userId'], payload['title'], (0, 1)[bool(payload['completed'])]])
+        sql = 'INSERT INTO todos (user_id, title, completed) VALUES ({0}, "{1}", {2})'.format(*tuple(data))
         c.execute(sql)
+
+        res = make_response(jsonify({
+            "id": c.lastrowid,
+            "userId": data[0],
+            "title": data[1],
+            "completed": bool(data[2])
+        }))
+        res.mimetype = 'application/json'
         c.close()
         conn.close()
-        res = make_response(jsonify(c.lastrowid))
-        res.mimetype = 'application/json'
         return res
+
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
