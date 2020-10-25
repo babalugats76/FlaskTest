499ba22ad2352f9500d03696612b7bee95f29251
James Colestock <james@colestock.com>
2020-10-24 23:33:09 -0600

    Start migrating to use of MongoDB

--- todo/__init__.py
+++ todo/__init__.py
@@ -2,10 +2,14 @@ from flask import Flask
 from flask_cors import CORS
 import os
 import logging
+from .JSONEncoder import JSONEncoder
+
 app = Flask(__name__)
 cors = CORS(app, resources={r'/api/*': {"origins": ["https://cdpn.io"]}})
 logging.basicConfig(level=logging.DEBUG)
 logging.getLogger('flask_cors').level = logging.DEBUG
+
 app.config['AUTHOR'] = os.environ.get('AUTHOR', None)
 app.config['MONGO_URI'] = os.environ.get('MONGO_URI', None)
+
 from todo import controllers
--- todo/controllers.py
+++ todo/controllers.py
@@ -1,17 +1,19 @@
-import sqlite3
+#import sqlite3
+
 from flask import make_response, jsonify, request, render_template
 from pymongo import MongoClient
+from todo import JSONEncoder
 from todo import app
 
-client = MongoClient(app.config['MONGO_URI'])
-db = client['flask-demo']
+app.json_encoder = JSONEncoder
+
 #todos = db['todos']
 
-def dict_factory(cursor, row):
+'''def dict_factory(cursor, row):
     d = {}
     for idx, col in enumerate(cursor.description):
         d[col[0]] = row[idx]
-    return d
+    return d'''
 
 @app.route('/', methods=['GET'])
 def index():
@@ -20,6 +22,30 @@ def index():
 @app.route('/api/todo/', methods=['POST'])
 @app.route('/api/todo/<int:id>', methods=['GET'])
 def todo(id=None):
+    client = MongoClient(app.config['MONGO_URI'])
+    db = client['flask-demo']
+    res = None
+    if request.method == 'GET':
+        data = db.todos.find_one({"id": id })
+        if data is None:
+            res = make_response(jsonify({}), 200)
+            res.headers['Content-Type'] = 'application/json; charset=utf-8'
+            return res
+        res = make_response(jsonify(data), 200)
+        res.headers['Content-Type'] = 'application/json; charset=utf-8'
+        return res
+    elif request.method == 'POST':
+        payload = request.get_json()
+        user_id = payload.get("userId", None)
+        title = payload.get("title", None)
+        completed = payload.get("completed", None)
+        if user_id is None or title is None or completed is None:
+            return make_response(jsonify({"message": "Bad request parameters"}), 400)
+        #data = db.todos.insert_one({ "id": 1, "userId": user_id, })
+        print(type(payload))
+        return make_response(jsonify({ "message": "complete this code..."}))
+
+    '''
     conn = sqlite3.connect('rest.db', isolation_level=None)
     conn.row_factory = dict_factory
     c = conn.cursor()
@@ -28,7 +54,7 @@ def todo(id=None):
         data = c.fetchone()
         if data is None:
             return make_response(jsonify({}))
-        data['completed'] = bool(data['completed'])
+        data['completed'] = bool(data['completed'])  
         res = make_response(jsonify(data))
         res.mimetype = 'application/json; charset=utf-8'
         c.close()
@@ -50,4 +76,5 @@ def todo(id=None):
         c.close()
         conn.close()
         return res
+     '''
 
